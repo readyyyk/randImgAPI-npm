26992fe0f8a6c494e761243ddeef6861cb078850
openaireadyk@gmail.com <mr.sairexer@gmail.com>
2023-07-14 12:34:39 +0300

    1.0.0

---
+++ HashMapsApi.js
@@ -0,0 +1,51 @@
+/**
+ * Client API for {@link https://github.com/readyyyk/randImgAPI}
+*/
+class HashMapApi {
+    models = ['hashmap', 'picsum'];
+
+    /**
+     * @constructor
+     * @param baseUrl {string}  - URL host that handles api
+     */
+    constructor(baseUrl) {
+        try {
+            const url = new URL(baseUrl);
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+        this.baseUrl = baseUrl;
+    }
+
+    #createLink(model, seed, w, h){
+        let result = this.baseUrl;
+        if (!this.models.includes(model)) {
+            console.error(`No such model - ${model}`);
+            return null;
+        }
+        result += `/${model}?`;
+        if (seed)
+            result += `seed=${seed}&`;
+        if (w)
+            result += `w=${w}&`;
+        if (h)
+            result += `h=${h}&`;
+        return result
+    }
+
+    /**
+     * Returns link with provided:
+     * @constructor
+     * @param model {"picsum" | "hashmaps"} - service to use
+     * @param [seed="Unix_nano"] {string} - seed to generate
+     * @param [width] {number} - width in pixels
+     * @param [height] {number} - height in pixels
+     * @returns {string | null} - link to image or null with console.error() called
+     */
+    getLink(model, seed, width, height) {
+        return this.#createLink(model, seed, width, height);
+    }
+}
+
+export default HashMapApi;
